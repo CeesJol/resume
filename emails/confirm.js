ff9093c8777acbbd4e63fe157769fed3fe739c89
Cees <cjol99@icloud.com>
2020-08-18 13:02:03 +0200

    💬 Update email text

--- emails/confirm.js
+++ emails/confirm.js
@@ -3,7 +3,7 @@ const Email = (token) => {
 	<div style="position: relative; margin: 0 auto; max-width: 400px;">
 	<h1>Affilas</h1>
 	<div style="background-color: white; border: 1px solid lightgrey; border-radius: 10px; margin: 0 auto; padding: 10px 10px 30px 10px;">
-	<p>Please verify your email address to start using Affilas.</p>
+	<p>By clicking on the following link, you are confirming your email address.</p>
 	<br /> <a href="${process.env.DOMAIN_NAME}/confirm/${token}"> <button style="background-color: green; border: none; color: white; padding: 10px; border-radius: 2px; width: 100%; cursor: pointer;">Confirm email address</button> </a> <br /><br />
 	<p>Or, alternatively, copy the link below in your browser:</p>
 	<a href="${process.env.DOMAIN_NAME}/confirm/${token}">${process.env.DOMAIN_NAME}/confirm/${token}</a></div>

0a7d882764697da1513b7f10f83810a70f99c85a
Cees <cjol99@icloud.com>
2020-11-25 08:55:46 +0100

    🚨 Fix yearpicker key

--- components/dashboard/pickers/Yearpicker.js
+++ components/dashboard/pickers/Yearpicker.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Yearpicker = ({ val, name, fn }) => {
-  const items = [<option value="">Year</option>];
+  const items = [
+    <option key={`yearpicker-${val}-${0}`} value="">
+      Year
+    </option>,
+  ];
   let currentYear = new Date().getFullYear();
   for (let i = currentYear; i >= 1960; i--) {
     items.push(
